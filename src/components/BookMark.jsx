001bdda9cf787f68e96a5c88a726b19d19d04bf8
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-10-24 06:46:54 +0600

    some visual changes

--- src/components/BookMark.jsx
+++ src/components/BookMark.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const BookMark = ({details}) => {
   const {id, title} = details;
   return (
-    <div className='p-5 bg-base-300 m-3'>
+    <div className='p-5 bg-base-300 m-3 rounded-lg'>
       <h3 className='font-bold text-lg'>Article #{id}: {title}</h3>
     </div>
   );
